629706956ca875aa6eea8ca43b67d8faccbde614
JoshuaGabriel <42494981+JoshuaGabriel@users.noreply.github.com>
2022-12-05 03:39:00 -0800

    testing encrypt function

--- tests/test_account.py
+++ tests/test_account.py
@@ -3,6 +3,8 @@ import unittest
 from main import *
 from Account import *
 import shutil
+from Rsa import *
+
 _root = os.getcwd() + "/Accounts/"
 
 class Test_AccountCreation():
@@ -10,12 +12,7 @@ class Test_AccountCreation():
     COMMANDS = ["", "login", "create_account", "delete_account", "chat", "delete_history", "exit", "help", "logout"]
     NUMARGS = [0, 3, 2, 2, 3, 2, 1, 1, 1]
 
-    
-    def test_login(self):
-        a,b = ParseCommand("login")
-        
-        assert False
-    
+
     def test_create_account(self):
 
         bob = Account("LordLucas", "1", "1", "1234")
@@ -42,31 +39,17 @@ class Test_AccountCreation():
         assert not os.path.isdir(_root + "LordLucas") 
         
         
-    def test_delete_account(self):
+    def test_encrypt(self):
+        
+        data = Encrypt("Lucas" + "\n" + "(724723, 1649057)" + "\n" + "localhost", "(730267, 1649057)")
+        
+        assert False
+
+    def test_decrypt(self):
         a,b = ParseCommand("delete_account")
         
         assert False
 
-    def test_chat(self):
-        a,b = ParseCommand("chat")
-        
-        assert False
-        
-    def test_exit(self):
-        a,b = ParseCommand("exit")
-        
-        assert False
-        
-    def test_help(self):
-        a,b = ParseCommand("help")
-        
-        assert False
-    
-    def test_logout(self):
-        a,b = ParseCommand("logout")
-        
-        assert False
-        
     
 
 
